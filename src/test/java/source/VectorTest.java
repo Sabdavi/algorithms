12921488cd525db29c7dd55073f002274ae446cf
Sabdavi <abdavi.saeed@gmail.com>
2021-11-13 18:48:35 +0100

    add simple test class

---
+++ src/test/java/source/VectorTest.java
@@ -0,0 +1,12 @@
+package source;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class VectorTest {
+    @Test
+    public void testVector(){
+        assertTrue(1==1);
+    }
+}
